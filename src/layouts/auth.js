e1e6c89e3b48dd6d090d97eee7e24fb1daa9cd91
Jedai Saboteur <paradox.inversion@gmail.com>
2022-10-23 11:54:11 -0700

    Added auth layout

---
+++ src/layouts/auth.js
@@ -0,0 +1,11 @@
+import { LogIn } from "../components/login"
+import { SignUp } from "../components/signup"
+
+export const Auth = () => {
+    return (
+        <div>
+            <SignUp />
+            <LogIn />
+        </div>
+    )
+}
