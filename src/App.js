8a789b0557c30d88f9d5d4931cfcd1d1b0974964
Jedai Saboteur <paradox.inversion@gmail.com>
2022-10-23 11:48:46 -0700

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Auth } from './layouts/auth';
+import { Inventory } from './layouts/inventory';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { NavigationBar } from './components/navigation';
 function App() {
+  const [currentTab, setCurrentTab] = useState("inventory");
+  const [inventoryItems, setInventoryItems] = useState([]);
+  
+  const getInventory = async () =>{
+
+    const result = await axios.get(`${process.env.REACT_APP_API_URL}/api/v1/inventory`);
+    setInventoryItems(result.data.inventoryItems)
+  }
+  useEffect(()=>{
+    getInventory();
+  }, [])
+
+  const onTabSelect = (selectedTab) =>{
+    setCurrentTab(selectedTab)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-blue-900 text-white">
+      <header>
+        <p className='text-3xl p-2 font-bold'>Waste Not</p>
       </header>
+      <div className="bg-blue-400 min-h-screen p-2">
+        {/* <NavigationBar onTabSelect={onTabSelect}/> */}
+        {currentTab === "authentication" && <Auth />}
+        {currentTab === "inventory" && <Inventory  inventory={inventoryItems} getInventory={getInventory} />}
+      </div>
     </div>
   );
 }
