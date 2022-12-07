8943c66570a5a5639542a8003a17a54a3c129faf
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 21:23:36 -0800

    Fixed className tag
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- src/components/modal.js
+++ src/components/modal.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const Modal = ({ isShowing, hide, children }) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
-    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
     <div className="fixed inset-0 z-10" aria-modal aria-hidden tabIndex={-1} role="dialog">
       <div className='min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
           <div className='p-4 relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
