b3bdaa84c8b6ae9ed7fa397f159b74e3449c4223
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 18:10:48 -0800

    Reworked form

--- src/components/inventoryItemForm.js
+++ src/components/inventoryItemForm.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from "yup";
+import useModal from "../hooks/useModal";
 import { Button } from "./button";
 
 const newItemSchema = Yup.object().shape({
@@ -8,6 +9,9 @@ const newItemSchema = Yup.object().shape({
     purchaseDate: Yup.date().required(),
     expirationDate: Yup.date(),
     useOrFreezeDate: Yup.date(),
+    opened: Yup.boolean(),
+    used: Yup.boolean(),
+    notes: Yup.string()
 });
 
 const updateItemSchema = Yup.object().shape({
@@ -15,21 +19,70 @@ const updateItemSchema = Yup.object().shape({
     purchaseDate: Yup.date(),
     expirationDate: Yup.date(),
     useOrFreezeDate: Yup.date(),
+    opened: Yup.boolean(),
+    used: Yup.boolean(),
+    notes: Yup.string()
 });
 
-export const InventoryItemForm = (props) =>{
+/**
+ * 
+ * @param {Object} params 
+ * @param {string} params.fieldName - The name of the field (for label and error association)
+ * @param {string} params.label - The text of the HTML label for the form field.
+ * @param {string} params.type - The input type of the field
+ * @param {string} params.fieldAs - How formik should render the field
+ * @param {Object} params.container
+ * @param {string} params.container.className - The html class string to use for this field's container. Defaults to `"mb-4 flex flex-col"`
+ * @param {string} params.field.className - The html class string to use for this field input. Defaults to `"input"`
+ * @returns 
+ */
+const inventoryFormField = (params) => {
+    const { 
+        fieldName,
+        label, 
+        type,
+        fieldAs
+    } = params;
+
+    const containerClass = params.container?.className;
+    const fieldClass = params.container?.className;
+    if (type === "checkbox") {
+        return (
+            <div className={`${containerClass || "mb-4 flex flex-col"}`}>
+                <div className="flex justify-between">
+                    <label htmlFor={fieldName}>{label}</label>
+                    <Field id={fieldName} name={fieldName} type="checkbox" className={`${fieldClass || "input"}`} />
+                </div>
+                <ErrorMessage name={fieldName} />
+            </div>
+        )
+    }
+    return (
+        <div className={`${containerClass || "mb-4 flex flex-col"}`}>
+            <label htmlFor={fieldName}>{label}</label>
+            <Field id={fieldName} name={fieldName} type={type || "text"} className={`${fieldClass || "input"}`} as={fieldAs || undefined} />
+            <ErrorMessage name={fieldName} />
+        </div>
+    )
+}
+
+export const InventoryItemForm = (props) => {
+    const { isShowing, toggle } = useModal();
     const {
         _id,
         name: InventoryItemName,
         purchaseDate,
         expirationDate,
-        useOrFreezeDate
-    }= props;
+        useOrFreezeDate,
+        opened,
+        used,
+        notes
+    } = props;
 
-    const onSubmitUpdate = async (values) =>{
+    const onSubmitUpdate = async (values) => {
         const body = {
             inventoryItemId: _id,
-            updateFields:{
+            updateFields: {
                 ...values
             }
         }
@@ -38,7 +91,7 @@ export const InventoryItemForm = (props) =>{
         props.close && props.close()
     }
 
-    const onSubmitNew = async (values) =>{
+    const onSubmitNew = async (values) => {
         const body = {
             createFields: {
                 ...values
@@ -54,51 +107,39 @@ export const InventoryItemForm = (props) =>{
         purchaseDate: "",
         expirationDate: "",
         useOrFreezeDate: "",
-    } : 
-    {
-        name: InventoryItemName,
-        purchaseDate: purchaseDate ? purchaseDate.slice(0,10) : "",
-        expirationDate: expirationDate ? expirationDate.slice(0,10) : "",
-        useOrFreezeDate: useOrFreezeDate ? useOrFreezeDate.slice(0,10) : "",
-    }
-    
+        opened: false,
+        used: false,
+        notes: ""
+    } :
+        {
+            name: InventoryItemName,
+            purchaseDate: purchaseDate ? purchaseDate.slice(0, 10) : "",
+            expirationDate: expirationDate ? expirationDate.slice(0, 10) : "",
+            useOrFreezeDate: useOrFreezeDate ? useOrFreezeDate.slice(0, 10) : "",
+            opened: opened ? opened : false,
+            used: used ? used: false,
+            notes: notes ? notes : ""
+        }
+
 
     return (
         <Formik
             initialValues={initialFormValues}
-            onSubmit={async (values)=>{
+            onSubmit={async (values) => {
                 props.create ? await onSubmitNew(values) : await onSubmitUpdate(values)
             }}
-            validationSchema={props.create ?  newItemSchema : updateItemSchema}
+            validationSchema={props.create ? newItemSchema : updateItemSchema}
         >
             <Form className="flex flex-col">
                 <section className="flex flex-col mb-4">
-                    <label htmlFor="name">Item*</label>
-                    <Field id="name" name="name" placeholder="name" className="input" />
-                    <ErrorMessage name="name" />
-
-                    <label htmlFor="purchaseDate">Purchase Date</label>
-                    <Field id="purchaseDate" name="purchaseDate" type="date" className="input" />
-                    <ErrorMessage name="purchaseDate" />
-
-                    <label htmlFor="expirationDate">Expiration Date</label>
-                    <Field id="expirationDate" name="expirationDate" type="date" className="input" />
-                    <ErrorMessage name="expirationDate" />
-
-                    <label htmlFor="useOrFreezeDate">Use or Freeze Date</label>
-                    <Field id="useOrFreezeDate" name="useOrFreezeDate" type="date" className="input" />
-                    <ErrorMessage name="useOrFreezeDate" />
-
-                    <label htmlFor="opened">Has the item been opened?</label>
-                    <Field id="opened" name="opened" type="checkbox" className="input" />
-                    <ErrorMessage name="opened" />
-
-                    <label htmlFor="used">Has the item been used?</label>
-                    <Field id="used" name="used" type="checkbox" className="input" />
-                    <ErrorMessage name="used" />
+                    { inventoryFormField({ fieldName: "name", label: "Item*" }) }
+                    { inventoryFormField({ fieldName: "purchaseDate", label: "Purchase Date", type: "date" }) }
+                    { inventoryFormField({ fieldName: "expirationDate", label: "Expiration Date", type: "date" }) }
+                    { inventoryFormField({ fieldName: "useOrFreezeDate", label: "Use or Freeze Date", type: "date" }) }
+                    { inventoryFormField({ fieldName: "notes", label: "Notes", type: "textarea", fieldAs: "textarea" }) }
+                    { inventoryFormField({ fieldName: "opened", label: "Has the item been opened?", type: "checkbox" }) }
+                    { inventoryFormField({ fieldName: "used", label: "Has the item been used?", type: "checkbox" }) }
                 </section>
-
-                {/* <button type="submit">Save</button> */}
                 <Button type="submit">Save</Button>
             </Form>
         </Formik>
