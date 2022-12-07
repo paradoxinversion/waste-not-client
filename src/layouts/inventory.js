0229edf6367780b913c3f3a3ca2acc5abb0d356f
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 21:24:05 -0800

    Added map key, hid falsey expirations
    Signed-off-by: Jedai Saboteur <paradox.inversion@gmail.com>

--- src/layouts/inventory.js
+++ src/layouts/inventory.js
@@ -42,9 +42,10 @@ export const Inventory = (props) => {
                 <header>Unexpired Food</header>
                 <div className="border rounded border-slate-500 p-2">
                     {inventory.filter(item => !item.used).map(item => (
-                        <div className="mb-4 border p-2">
+                        <div key={item._id} className="mb-4 border p-2">
                             <p className="">{item.name}</p>
-                            <p className="">Expiration: {item.expirationDate?.slice(0, 10)}</p>
+                            {item.expirationDate && <p className="">Expiration: {item.expirationDate?.slice(0, 10)}</p>}
+                            
                             {item.opened && (
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
