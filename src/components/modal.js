95e9fda040428f552c51eda711a33f32c00069d0
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 18:10:55 -0800

    Reworked modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -4,22 +4,18 @@ import ReactDOM from 'react-dom';
 const Modal = ({ isShowing, hide, children }) => isShowing ? ReactDOM.createPortal(
   <React.Fragment>
     <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-    <div className="relative z-10" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="fixed inset-0 z-10 overflow-y-auto">
-        <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
-            <div className='p-4 relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-
-                <div className="modal-header">
-                <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                </div>
-                {children}
-            </div>
-        </div>
+    <div className="fixed inset-0 z-10" aria-modal aria-hidden tabIndex={-1} role="dialog">
+      <div className='min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+          <div className='p-4 relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+              <div className="modal-header">
+              <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+                  <span aria-hidden="true">&times;</span>
+              </button>
+              </div>
+              {children}
+          </div>
       </div>
     </div>
   </React.Fragment>, document.body
 ) : null;
-
 export default Modal;
