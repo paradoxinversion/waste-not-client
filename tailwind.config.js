d20cef5eface3b10657b9e4ea40675bf02003001
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 18:09:47 -0800

    Added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "eerieBlack": "#131515",
+        "jet": "#2b2c28",
+        "snow": "#FFFAFB",
+        "cafeAuLait": "#A7754D",
+        "darkOrange": "#FF8811"
+      }
+    },
   },
   plugins: [],
 }
