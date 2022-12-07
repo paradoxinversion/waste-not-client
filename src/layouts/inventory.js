fc2cef4d8ae1bc21c29429630fd5dcee5ceb823c
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 18:11:08 -0800

    Reworked inventory display

--- src/layouts/inventory.js
+++ src/layouts/inventory.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Modal from "../components/modal";
 import useModal from "../hooks/useModal";
 import { InventoryItemForm } from "../components/inventoryItemForm";
@@ -7,83 +7,66 @@ import axios from "axios";
 
 
 export const Inventory = (props) => {
-    const {isShowing, toggle} = useModal();
+    const { isShowing, toggle } = useModal();
     const [inventoryFormCreate, setInventoryFormCreate] = useState(true)
     const [selectedItem, setSelectedItem] = useState(null);
+    const [showUnexpiredFood, setShowUnexpiredFood] = useState(true)
     const { inventory } = props;
 
     return (
-        <div className="overflow-y-auto min-h-1/2 min-w-screen text-blue-50">
+        <div className="overflow-y-auto">
             <Modal
                 isShowing={isShowing}
                 hide={toggle}
             >
-                <InventoryItemForm create={inventoryFormCreate} cb={props.getInventory} {...selectedItem}/>
+                <InventoryItemForm
+                    create={inventoryFormCreate}
+                    close={toggle}
+                    cb={props.getInventory}
+                    {...selectedItem} />
             </Modal>
 
-            <header className="mb-4">
+            <header className="mb-4 text-cafeAuLait">
                 <h1 className="text-3xl font-bold">Inventory</h1>
             </header>
             <div className="mb-4">
-                <Button 
-                    onClick={()=>{
+                <Button
+                    onClick={() => {
                         setInventoryFormCreate(true);
                         toggle();
-                }}>
+                    }}>
                     New Item
                 </Button>
             </div>
-
             <div>
-                {(!inventory || (inventory && inventory.length === "0")) ?
-                    <p>No items</p> : 
-                    <div>
-                        <table className=" table-fixed border-separate border rounded border-slate-500 bg-blue-300 p-2">
-                            <thead>
-
-                                <tr>
-                                    <th className="border border-slate-600 p-1">Name</th>
-                                    <th className="border border-slate-600 p-1">Purchase Date</th>
-                                    <th className="border border-slate-600 p-1">Expiration Date</th>
-                                    <th className="border border-slate-600 p-1">Use or Freeze Date</th>
-                                    <th className="border border-slate-600 p-1">Opened</th>
-                                    <th className="border border-slate-600 p-1">Used</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                {inventory.map(inventoryItem => (
-                                    <tr key={inventoryItem._id} className={inventoryItem.expirationDate && new Date(inventoryItem.expirationDate) < new Date(Date.now()) ? "text-red-500" : ""}>
-                                        <td className="border border-slate-600 p-1 p-1">{inventoryItem.name}</td>
-                                        <td className="border border-slate-600 p-1">{inventoryItem.purchaseDate?.slice(0,10)}</td>
-                                        <td className="border border-slate-600 p-1">{inventoryItem.expirationDate?.slice(0,10)}</td>
-                                        <td className="border border-slate-600 p-1">{inventoryItem.useOrFreezeDate ? new Date(inventoryItem.useOrFreezeDate).toLocaleDateString() : "---"}</td>
-                                        <td className="border border-slate-600 p-1 p-1">{inventoryItem.opened ? "Yes" : "No"}</td>
-                                        <td className="border border-slate-600 p-1 p-1">{inventoryItem.used ? "Yes" : "No"}</td>
-                                        <td className="border border-slate-600 p-1">
-                                            <Button onClick={()=>{
-                                                setSelectedItem(inventoryItem);
-                                                setInventoryFormCreate(false);
-                                                toggle();
-                                            }} >
-                                                Edit
-                                            </Button>
-                                            
-                                        </td>
-                                        <td className="border border-slate-600 p-1">
-                                            <Button onClick={async ()=>{
-                                                await axios.delete(`${process.env.REACT_APP_API_URL}/api/v1/inventory`, {
-                                                    inventoryItemId: inventoryItem._id
-                                                })
-                                                props.getInventory && props.getInventory();
-                                            }} >Delete</Button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                }
+                <header>Unexpired Food</header>
+                <div className="border rounded border-slate-500 p-2">
+                    {inventory.filter(item => !item.used).map(item => (
+                        <div className="mb-4 border p-2">
+                            <p className="">{item.name}</p>
+                            <p className="">Expiration: {item.expirationDate?.slice(0, 10)}</p>
+                            {item.opened && (
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            )}
+                            <p>{item.notes}</p>
+                            <Button additonalStyles="mr-4" onClick={()=>{
+                                setSelectedItem(item);
+                                setInventoryFormCreate(false);
+                                toggle();
+                            }}>Edit</Button>
+                            <Button onClick={async ()=>{
+                                    await axios.delete(`${process.env.REACT_APP_API_URL}/api/v1/inventory`, {
+                                        data: {
+                                            inventoryItemId: item._id
+                                        }
+                                    })
+                                props.getInventory && props.getInventory();
+                            }}>Delete</Button>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
