cf5a6010f94b7f356523c5553353f97deb834213
Jedai Saboteur <paradox.inversion@gmail.com>
2022-12-06 18:10:34 -0800

    Restyled button

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,6 @@
 export const Button = (props) => {
-    const {children, onClick, type} = props;
+    const {children, onClick, type, additonalStyles} = props;
     return (
-        <button className="btn" type={type} onClick={onClick}>{children}</button>
+        <button className={`btn ${additonalStyles}`} type={type} onClick={onClick}>{children}</button>
     )
 }
